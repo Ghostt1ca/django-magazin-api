2e89f8347949dad387c3454b8ec94c4799724f9a
Ghostt1ca <emanuelandreisteleac@gmail.com>
2026-01-09 15:27:48 +0200

    Force migrations in WSGI

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# Aceasta linie ruleaza migrarile la fiecare pornire de proces
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Migration failed: {e}")
 
 application = get_wsgi_application()
